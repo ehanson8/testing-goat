5882d8ce0e2a6d1baa890d476b734246c2797f9e
Eric Hanson <ehanson@mit.edu>
2019-04-05 11:36:41 -0400

    liveServer

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -6,7 +7,7 @@ from selenium.webdriver.support.expected_conditions import staleness_of
 from contextlib import contextmanager
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -25,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -75,8 +76,3 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
-
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
